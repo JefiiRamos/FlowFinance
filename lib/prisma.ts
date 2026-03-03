7c3a147c597bc055568f9281931047ebaa197579
DinizGui <guicdiniz.gui@gmail.com>
2026-03-03 19:39:41 -0300

    up

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,5 @@
-import type { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg'
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient | undefined }
 
@@ -8,9 +9,6 @@ function getPrisma(): PrismaClient {
   if (!connectionString) {
     throw new Error('DATABASE_URL is not defined')
   }
-  // Dynamic require to avoid loading pg/adapter during build (causes "Failed to collect page data")
-  const { PrismaPg } = require('@prisma/adapter-pg')
-  const { PrismaClient } = require('@prisma/client')
   const adapter = new PrismaPg({ connectionString })
   const client = new PrismaClient({ adapter })
   globalForPrisma.prisma = client
